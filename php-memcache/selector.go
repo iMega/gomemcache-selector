2bd315ebd38158277d43a1d9df91c8cde5a76e91
Dmitry Stoletov <irvis@imega.ru>
2021-06-11 16:00:02 +0300

    Memcache consistent hash portable for Go
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- php-memcache/selector.go
+++ php-memcache/selector.go
@@ -1,4 +1,4 @@
-// Copyright © 2020 Dmitry Stoletov <info@imega.ru>
+// Copyright © 2021 Dmitry Stoletov <info@imega.ru>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
